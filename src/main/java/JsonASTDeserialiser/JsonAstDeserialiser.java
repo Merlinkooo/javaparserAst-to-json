d6765c24ec81b36fd72441acfb9af5c100dad875
MakuchJakub <makuch5@stud.uniza.sk>
2025-02-11 11:54:15 +0100

    another visit methods firstDraft

--- src/main/java/JsonASTDeserialiser/JsonAstDeserialiser.java
+++ src/main/java/JsonASTDeserialiser/JsonAstDeserialiser.java
@@ -1,81 +1,131 @@
 package JsonASTDeserialiser;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ContainerNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
 import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
-public class JsonAstDeserialiser extends VoidVisitorAdapter<FileWriter> {
+public class JsonAstDeserialiser extends GenericVisitorAdapter<JsonNode,JsonNode> {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    /*
+    private <N extends Node> void addElementsToArray(ArrayNode array, List<N> elements){
+        for (Node element : elements){
+            ObjectNode jsonNode = objectMapper.createObjectNode();
+            this.visit(element,jsonNode);
+            array.add(jsonNode);
+
+        }
+    }*/
+    /*ass second argument should be ArrayNode
+    */
     @Override
-    public void visit(MethodDeclaration n, FileWriter arg) {
+    public JsonNode visit(MethodDeclaration n, JsonNode arg) {
         super.visit(n, arg);
-        try {
-            arg.write("{\n");
-            arg.write("\"NodeType\" : \"MethodDeclaration\"\n");
-            arg.write("\"Name\" : \"" + n.getName() +"\",\n");
-            arg.write("\"ReturnType\": ");
-            n.getType().accept(this,arg);
-            arg.write("\n\"Modifiers\": [");
+            if (!(arg instanceof ArrayNode)) return null;
+            ObjectNode method = objectMapper.createObjectNode();
+            method.put("node_type","MethodDeclaration");
+            method.put("name",n.getName().asString());
+
+            method.put("return_type",n.getType().accept(this,method));
+
+            ArrayNode modifiers = objectMapper.createArrayNode();
+            method.put("modifiers",modifiers);
             for(Modifier modifier : n.getModifiers()){
-                arg.write("{ ");
-                this.visit(modifier,arg);
-                arg.write("}");
-                if(!modifier.equals(n.getModifiers().getLast())) arg.write(", ");
+                modifiers.add(visit(modifier,modifiers));
             }
-            arg.write("],\n");
-            arg.write("\"Parameters\" : [");
+            ArrayNode parameters = objectMapper.createArrayNode();
+
             for (Parameter parameter : n.getParameters()){
                 this.visit(parameter,arg);
+                parameters.add(this.visit(parameter,arg));
             }
-            arg.write("],\n");
+
+            ObjectNode bodyNode = objectMapper.createObjectNode();
             this.visit(n.getBody().get(),arg);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            method.put("body",bodyNode);
+
+            return method;
     }
 
     @Override
-    public void visit(Modifier n, FileWriter arg) {
+    public JsonNode visit(Modifier n, JsonNode arg) {
         super.visit(n, arg);
-        try {
-            arg.write("\"NodeType : \"Modifier\",\n");
-            arg.write("\"Name\" : ");
-            arg.write("\"" + n.getKeyword().asString() + "\"");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        TextNode textNode = TextNode.valueOf(n.toString());
+        return textNode;
+
     }
 
     @Override
-    public void visit(Parameter n, FileWriter arg) {
+    public JsonNode visit(Parameter n, JsonNode arg) {
         super.visit(n, arg);
-        try {
-            arg.write("{\n");
-            arg.write("\"NodeType :\" : \"" + "Parameter\",\n");
-            arg.write("\"Name\" : \"" + n.getName() + "\", ");
-            n.getType().accept(this,arg);
-            arg.write("\n\"Modifiers\": [");
+            ObjectNode parameterNode = objectMapper.createObjectNode();
+            parameterNode.put("nodeType","Parameter");
+            parameterNode.put("name",n.getName().toString());
+            //typ musi pridat do argumentu arg svoj typ;
+            n.getType().accept(this,parameterNode);
+            ArrayNode arrayNode = objectMapper.createArrayNode();
+
             for(Modifier modifier : n.getModifiers()){
-                arg.write("{ ");
-                this.visit(modifier,arg);
-                arg.write("}");
-                if(!modifier.equals(n.getModifiers().getLast())) arg.write(", ");
+                this.visit(modifier,arrayNode);
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        n.getType().accept(this,arg);
+        return parameterNode;
 
     }
 
     @Override
-    public void visit(ClassOrInterfaceType n, FileWriter arg) {
+    public JsonNode visit(ClassOrInterfaceType n, JsonNode arg) {
         super.visit(n, arg);
+        TextNode textNode = new TextNode(n.toString());
+        return textNode;
     }
 
+    @Override
+    public JsonNode visit(ClassOrInterfaceDeclaration n, JsonNode arg) {
+        //return super.visit(n, arg);
+        ObjectNode classOrInterfDecl = objectMapper.createObjectNode();
+        classOrInterfDecl.put("node_type","ClassOrInterfaceDeclaration");
+        ArrayNode modifiers = objectMapper.createArrayNode();
+        classOrInterfDecl.put("modifiers",modifiers);
+        n.getModifiers().forEach(mod -> modifiers.add(this.visit(mod,null)));
+
+
+        ArrayNode attributes = objectMapper.createArrayNode();
+        classOrInterfDecl.put("attributes",attributes);
+        n.getFields().forEach(field -> attributes.add(this.visit(field,null)));
+
+
+        ArrayNode methods = objectMapper.createArrayNode();
+        classOrInterfDecl.put("methods",methods);
+        n.getMethods().forEach(m -> methods.add(visit(m,null)));
+        return classOrInterfDecl;
+    }
+    public String convertToJson(final Node node){
+        JsonNode jsonRepresentation = node.accept(this,null);
+
+        try{
+            return objectMapper.writeValueAsString(jsonRepresentation);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 }
